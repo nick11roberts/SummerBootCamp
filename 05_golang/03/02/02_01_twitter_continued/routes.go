4c95c73d5a8311784373114d85f46a08bc3c802a
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2015-07-21 16:24:01 -0700

    Fixed minor bug in which, when deployed, profiles are not displayed.

--- 05_golang/03/02/02_01_twitter_continued/routes.go
+++ 05_golang/03/02/02_01_twitter_continued/routes.go
@@ -9,7 +9,7 @@ func init() {
 	http.HandleFunc("/tweet", tweet)
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/logout", logout)
-	http.HandleFunc("/profile/", profile)
+	http.HandleFunc("/profile", profile)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public/"))))
 }
